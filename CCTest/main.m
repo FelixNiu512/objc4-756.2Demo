385901c97b33208ef65d64b6db7f777ea09e3a9e
Niu Chaohua <niuchaohua@foxmail.com>
2020-02-01 19:39:33 +0800

    runtime添加方法

--- CCTest/main.m
+++ CCTest/main.m
@@ -48,32 +48,15 @@
 
 @end
 
-@interface Person (Fly)
-
-- (void)fly;
-
-@end
-
-@implementation Person (Fly)
-
-- (void)fly {
-    NSLog(@"I am flying.");
-}
-
-@end
-
-void printProperties(Class cls) {
-    if (cls == nil) {
-        return ;
-    }
-    CCNSLog(@"------------ print %@ properties ------------", NSStringFromClass(cls));
-    uint32_t count;
-    objc_property_t *properties = class_copyPropertyList(cls, &count);
-    for (uint32_t i = 0; i < count; i++) {
-        objc_property_t property = properties[i];
-        CCNSLog(@"名字：%s -- 属性：%s", property_getName(property), property_getAttributes(property));
-    }
-}
+//@interface Tool : NSObject
+//
+//@end
+//
+//@implementation Tool
+//
+//
+//
+//@end
 
 void printMethods(Class cls) {
     if (cls == nil) {
@@ -88,36 +71,16 @@ void printMethods(Class cls) {
     }
 }
 
-id getter(id obj, SEL sel) {
-    return objc_getAssociatedObject(obj, (__bridge const void * _Nonnull)(NSStringFromSelector(sel)));
-}
-
-void setter(id obj, SEL sel, id value) {
-    objc_setAssociatedObject(obj, (__bridge const void * _Nonnull)(NSStringFromSelector(sel)), value, OBJC_ASSOCIATION_RETAIN);
-}
-
-void runtimeAddPropertyCountry(Class cls) {
-    objc_property_attribute_t attrs[] = {
-        { "T", "@\"NSString\"" },           // T 类型，这里country是NSString类型
-        { "&", "" },                        // & = strong/retain
-        { "N", "" },                        // N = nonatomic
-        { "V", "_country" }                 // V 成员变量
-    };
-    class_addProperty(cls, "country", attrs, 4);
-    
-    class_addMethod(cls, NSSelectorFromString(@"country"), (IMP)getter, "@@:");
-    class_addMethod(cls, NSSelectorFromString(@"setCountry:"), (IMP)setter, "v@:@");
+void fly(id obj, SEL sel) {
+    NSLog(@"I am flying");
 }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Class pcls = [Person class];
-        
-//        runtimeAddPropertyCountry(pcls);
-//        printProperties(pcls);
-//        printMethods(pcls);
-        
-        NSLog(@"%p", pcls);
+        class_addMethod(pcls, NSSelectorFromString(@"fly"), (IMP)fly, "v@:");
+        printMethods(pcls);
+        [[pcls alloc] performSelector:NSSelectorFromString(@"fly")];
     }
     return 0;
 }
